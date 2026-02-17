60505997b076a32b4dd02a2678d77b6cbcb813ba
Idelfrides Jorge <eng.idelfridesjorge@gmail.com>
2026-02-17 07:23:03 -0300

    feat|fix: add some updetes to make this classe better

--- LearningCsharp/ModelForConsoleApp.cs
+++ LearningCsharp/ModelForConsoleApp.cs
@@ -3,6 +3,8 @@
 
     MAKE THE DESCRIPTION OF YOUR CODE HERE 
 
+    :::> COPY THIS FILE AND THEN EDIT IT FOR YOU PURPOSE  <:::
+
 */
 
 
@@ -17,46 +19,50 @@ namespace LearningCsharp
 {    
 
    public class ModelForConsoleApp
-    {
+   {
         public ModelForConsoleApp() { }
 
         public static void MainMCA(string[] args)
         {
 
-            #region block ------------------ START OF APP ----------------------
+            #region block code --------- START OF APP ----------------------
 
-            // Create an instance of your custom logger
+            // Instance section
+            // In this section, you can instantiate all the classes you will use in the application.
             JCustomLogger logger = new("ModelForConsoleApp");
-
             AppInputOutputManager iom = new();
+
+            // Value definition for Constants and fields section
             var infos = "TITLE OF YOU APP HERE";
             char lineShape = '*';
             int lineLen = 0;
 
+            // Instance member call section
             iom.ShowAppInfo(
                 title: infos,
                 summary: "This App attempts to show how we can use Special type - record in C#."
             );
-            #endregion
+
+            #endregion // --- starting region ends here ---
 
 
-            #region block ------------------ THE LOGIC OF APP GOES HERE ----------------------
+            #region block code --------- THE LOGIC OF APP GOES HERE --------
 
             logger.Info("Application starting...");
 
             // THE LOGIC OF APP GOES HERE
             infos = "THE LOGIC OF APP GOES HERE";
             iom.ShowPrettyInfo(infos, lineShape, lineLen, 2, 0, false);
-            
-
-            #endregion
 
 
-            #region block ------------------ Conclusion: END OF APP --------------------
+            #endregion // --- the real app region ends here ---
+
+
+            #region block code --------- END OF APP ------------------------
 
             iom.ShowAppEndInfo();
 
-            #endregion
+            #endregion // --- closing region ends here ---
 
         }
     }
